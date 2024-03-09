c93b61810a7df3ed9ed5853a5abc36af87876971
Théo m <>
2024-03-09 21:57:58 +0100

    feat : configuration de la navbar

--- app/page.js
+++ app/page.js
@@ -1,17 +1,32 @@
+"use client";
 import styles from "./page.module.css";
 import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Navbar from "@/app/component/navbar";
 
 export default function Home() {
-  return (
-      <header className={styles.headerPart}>
-          <p>Unity</p>
-          <Stack spacing={2} direction="row">
-              <Button variant="text">Text</Button>
-              <Button variant="contained">Contained</Button>
-              <Button variant="outlined">Outlined</Button>
-          </Stack>
-      </header>
-  );
+    return (
+        <div>
+            <header id="accueil"  className={styles.headerPart}>
+                <Navbar/>
+            </header>
+            <main>
+                <div id="projet" className={styles.projet}>
+                    <h1>Unity</h1>
+                    <p>Unity</p>
+                    <Stack spacing={2} direction="row">
+                        <Button variant="text">Text</Button>
+                        <Button variant="contained">Contained</Button>
+                        <Button variant="outlined">Outlined</Button>
+                    </Stack>                </div>
+                <div id="solutions" className={styles.solutions}>
+                    part2
+                </div>
+                <div id="video" className={styles.video}>
+                    part3
+                </div>
+            </main>
+        </div>
+    );
 }
