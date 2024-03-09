b948c2a5c5d77c95e7b9bc2dd5007514481c4cf5
Théo m <>
2024-03-09 15:21:48 +0100

    fix : allègement du css

--- app/page.js
+++ app/page.js
@@ -3,11 +3,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-      <main className={styles.main}>
+      <header className={styles.headerPart}>
           <p>Unity</p>
-          <p>Unity</p>
-          <p>Unity</p>
-          <p>Unity</p>
-      </main>
+      </header>
   );
 }
