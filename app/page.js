1aa8fc48522c67189ea408ffdedefd1564a1f048
Théo m <>
2024-03-09 15:33:08 +0100

    feat : installation de MUI

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import * as React from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 export default function Home() {
   return (
       <header className={styles.headerPart}>
           <p>Unity</p>
+          <Stack spacing={2} direction="row">
+              <Button variant="text">Text</Button>
+              <Button variant="contained">Contained</Button>
+              <Button variant="outlined">Outlined</Button>
+          </Stack>
       </header>
   );
 }
